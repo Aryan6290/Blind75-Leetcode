3f5d3ff80880622c7d9229990297d0ccbaf97752
Aryan Barnwal <aryanbarnwal007@gmail.com>
2022-01-21 11:33:48 +0530

    Buy and Sell Stock DP

---
+++ Arrays/buyAndSellStock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        if (prices.size() == 1)
+            return 0;
+        int n = prices.size();
+        int dp[n];
+        dp[0] = nums[0];
+        int minValue = 0, maxValue = 0;
+        for (int i = 1; i < n; i++)
+        {
+            dp[i] = max(nums[i] - dp[i - 1])
+        }
+    }
+};
